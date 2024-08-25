b1f8a2ca5c5fd680a53b151061f1d4810563ca36
simarjeet21 <121251719+simarjeet21@users.noreply.github.com>
2024-08-25 17:39:58 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const SID = 22016033
+// SID= 2204 not allowed
+let email = "simsim@gmail.com"
+var city = "patiala"
+state = "punjab"
+let subscriber
+
+/*Avoid var; use let instead
+becoz var has issue with scope*/
+
+email= 'dfh@.com'
+state='PANJAB'
+
+console.log(SID);
+console.table([SID,email,city,state, subscriber])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let name = "sima" 
+let age= 21
+let subscriber= false
+
+console.log(typeof age);
+console.log(typeof name);
+
+// Number
+// BigInt
+// String
+// Boolean
+// null -> stamd-alone value
+// undefined
+// Symbol -> unique component characterstic eg: figma tools
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined); // undefined 
+
