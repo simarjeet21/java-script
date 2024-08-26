0ad7112024130a8f7f13d433c7c0685c085a9743
simarjeet21 <121251719+simarjeet21@users.noreply.github.com>
2024-08-26 09:58:35 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -13,3 +13,28 @@ let test2 = undefined
 let num2= Number(test2)
 console.log(num2) //undefined is better, results NaN
  
+//OPERATIONS
+let st1 = "hello"
+let st2 = " simar"
+console.log(st1+st2)
+
+console.log(1+"2");
+console.log(1+1+"2"); //first datatype is prefered
+console.log("1"+1+2);
+console.log("1"+1+"2");
+console.log(1+2);
+
+//COMPARISON
+console.log(1>2);
+console.log(1>=2);
+
+console.log(null>0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+// for undefined all false
+
+console.log(2=='2'); //data type conversion then compare -> true
+console.log(9==='9'); //strict check -> false
+
+
+
