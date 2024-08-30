607bb4aabada3172376455bc653b1a75ce35ab21
simarjeet21 <121251719+simarjeet21@users.noreply.github.com>
2024-08-30 17:05:30 +0000

    array_1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,3 +1,42 @@
 //array
 const arr = [1,4,2,3,69]
+console.log(arr[4]);
+
+arr.push("33 helllo")
+arr.push(123)
+arr.push(0.2)
 console.log(arr);
+
+arr.pop()
+arr.pop()
+console.log(arr);
+
+//unshift adds in the start of arr
+arr.unshift(888)
+console.log(arr);
+//shift removes that initial
+arr.shift()
+console.log(arr);
+
+// join adds all elements into a string
+const newArr= arr.join()
+console.log(newArr);
+
+//SLICE -> extracts the required and SPLICE -> removes the required from original arr
+const aaa=[2,3,4,5,6]
+
+const bbb= aaa.slice(1,3) //3 excluded and orig arr asitis
+console.log(bbb);
+console.log(aaa);
+
+const ccc= aaa.splice(1,3) //3 included and removes from orig arr
+console.log(ccc);
+console.log(aaa);
+
+
+
+
+
+
+
+
